61a0eb9a41f1608f18dcbab8716df7f391ecd011
alex-machulski <aliaksandr.machulski@gmail.com>
2021-01-10 21:15:40 +0300

    Updates

---
+++ precache-manifest.fb33bb260a6756275b70f72f2b5d383f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3475cbcba27f50d15372c01874c3d5",
+    "url": "/React-Homeworks/index.html"
+  },
+  {
+    "revision": "0a365c8475fff82ea64b",
+    "url": "/React-Homeworks/static/css/main.c69b3c5f.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/React-Homeworks/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Homeworks/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a365c8475fff82ea64b",
+    "url": "/React-Homeworks/static/js/main.4ea63734.chunk.js"
+  },
+  {
+    "revision": "6b64bf91255c5989209f",
+    "url": "/React-Homeworks/static/js/runtime-main.4d74a411.js"
+  }
+]);
