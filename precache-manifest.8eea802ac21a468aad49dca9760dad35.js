c55509ac62adc25965a9b2e0dd05707ee6fd70f4
alex-machulski <aliaksandr.machulski@gmail.com>
2021-02-17 14:25:29 +0300

    Updates

--- precache-manifest.8eea802ac21a468aad49dca9760dad35.js
+++ precache-manifest.8eea802ac21a468aad49dca9760dad35.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d48433c3077d8211cb3202a9e916811",
+    "revision": "4e8969f7f058f2bda400eb28dcb42768",
     "url": "/React-Homeworks/index.html"
   },
   {
-    "revision": "ab869f6b1500a63aa264",
+    "revision": "347cc879c36ac62f23d1",
     "url": "/React-Homeworks/static/css/main.7629631f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Homeworks/static/js/2.9dadcfcc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ab869f6b1500a63aa264",
-    "url": "/React-Homeworks/static/js/main.111da563.chunk.js"
+    "revision": "347cc879c36ac62f23d1",
+    "url": "/React-Homeworks/static/js/main.6536bf42.chunk.js"
   },
   {
     "revision": "6b64bf91255c5989209f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Homeworks/precache-manifest.144bf89008e6977eab5295efcb959157.js"
+  "/React-Homeworks/precache-manifest.8eea802ac21a468aad49dca9760dad35.js"
 );
 
 self.addEventListener('message', (event) => {
